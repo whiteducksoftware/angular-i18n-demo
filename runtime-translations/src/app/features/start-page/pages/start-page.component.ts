292005ca6c92f41799b68ca6cddbeb6f719ad2c1
Markus Voit <markus.voit@whiteduck.de>
2022-07-20 14:01:32 +0200

    feat: update i18n example for next blog post

---
+++ runtime-translations/src/app/features/start-page/pages/start-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-page',
+  templateUrl: './start-page.component.html',
+  styleUrls: ['./start-page.component.scss'],
+})
+export class StartPageComponent {}
---
+++ runtime-translations/src/app/features/start-page/start-page.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { StartPageComponent } from './pages/start-page.component';
+
+@NgModule({
+  declarations: [StartPageComponent],
+  imports: [CommonModule],
+})
+export class StartPageModule {}
